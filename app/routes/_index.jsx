763010009e1afdea4d63ff1b8cdd595f9e668c8c
Jim Kleckner <jim@importantlabs.com>
2024-08-11 17:06:00 -0700

    redo on remix

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -17,20 +17,20 @@ export default function Index() {
     to-yellow-500
     background-animate h-screen w-full flex items-center justify-center">
       <div className="sm:text-xl md:text-5xl text-center">
-        <h1 className="uppercase font-mono rotate-2 font-semibold text-yellow-300/20">
+        <h1 className="uppercase font-mono -rotate-2 font-semibold text-yellow-300/20">
           slippy jenkins
         </h1>
-        <h1 className="uppercase font-mono rotate-2 font-bold  text-pink-600/40">
+        <h1 className="uppercase font-mono -rotate-2 font-bold  text-pink-600/40">
           slippy jenkins
         </h1>
-        <h1 className="uppercase font-mono rotate-2 font-black text-white/20">
+        <h1 className="uppercase font-mono -rotate-2 font-black text-white/20">
           slippy jenkins
         </h1>
         <Link
           to="/dashboard"
           className="font-mono text-3xl opacity-50 mt-2 block border-2 py-1 px-3
-            border-pink-600/40 text-center rotate-2 hover:text-white">
-          Dashboard 0.1
+            border-pink-600/40 text-center -rotate-2 hover:text-white">
+          Dashboard 0.2 on Remix
         </Link>
       </div>
     </div>
